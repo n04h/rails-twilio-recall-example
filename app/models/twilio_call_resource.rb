4fa896b1a5ca31a6322593428b368d45910b907d
n04h <contact@n04h.jp>
2021-12-14 22:35:20 +0900

    feat: モデル回り実装

--- app/models/twilio_call_resource.rb
+++ app/models/twilio_call_resource.rb
@@ -1,2 +1,29 @@
 class TwilioCallResource < ApplicationRecord
+  class << self
+    def call!(client:, **call_options)
+      resource = client.calls.create(**call_options)
+      create!(
+        call_sid: resource.sid,
+        call_options_json: call_options.to_json
+      )
+    end
+  end
+
+  def call_options
+    JSON.parse(call_options_json).symbolize_keys
+  end
+
+  def can_recall?
+    called_count < 3
+  end
+
+  def recall!(client:)
+    raise "リコールできません" if can_recall?
+
+    resource = client.calls.create(**call_options)
+    update!(
+      call_sid: resource.sid,
+      called_count: called_count + 1
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :twilio_api do
+    resources :calls, only: :create
+  end
 end
