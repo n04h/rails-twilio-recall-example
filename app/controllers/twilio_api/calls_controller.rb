9464c6b9b50e583c8b3353bbcef127ecb94f865d
n04h <contact@n04h.jp>
2021-12-14 22:51:09 +0900

    feat: コールバックされるようにした

--- app/controllers/twilio_api/calls_controller.rb
+++ app/controllers/twilio_api/calls_controller.rb
@@ -7,16 +7,23 @@ module TwilioApi
     private
 
     def client
-      Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"])
+      Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
     end
 
     def call_options
       {
         twiml: '<Response><Say>Hello world!</Say></Response>',
+        status_callback: status_callback_url,
+        status_callback_event: ['answered', 'completed'],
+        status_callback_method: 'POST',
         from: '+8105012345678',
         to: '+8109012345678',
         timeout: 30 # デフォルトは60秒なので2回コールされてしまうのを防ぐ
       }
+    end
+
+    def status_callback_url
+      "#{ENV["HOST_URL"]}/twilio_api/callback"
     end
   end
 end
